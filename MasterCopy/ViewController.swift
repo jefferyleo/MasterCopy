33fce82b242989b59b05e4462fbc38b915d846f3
jefferyleo <jefferyleo93@hotmail.com>
2015-09-02 19:31:59 +0800

    Initial Commit

---
+++ MasterCopy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MasterCopy
+//
+//  Created by jeffery leo on 9/2/15.
+//  Copyright (c) 2015 jeffery leo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
